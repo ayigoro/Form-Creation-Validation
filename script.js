1ec8908539c6d7a19f6dfa491464e6625025b313
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-10-18 18:24:22 +0100

    form validation files submitted

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ document.addEventListener("DOMContentLoaded",function(){
  form.addEventListener("submit",(event)=>{
   event.preventDefault()
  })
- const usernameInput = document.getElementById("username").value.trim()
- const emailInput = document.getElementById("email").value.trim()
- const passwordInput = document.getElementById("password").value.trim()
+ const username = document.getElementById("username").value.trim()
+ const email = document.getElementById("email").value.trim()
+ const password = document.getElementById("password").value.trim()
 
  const isValid = true
  const messages = []
 
- if (usernameInput.length < 3){
+ if (username.length < 3){
   isValid =false
   messages.push("username must be at least more than 3 character long")
  }
 
- if (!emailInput.include("@") || !email.include(".")){
+ if (!email.include("@") || !email.include(".")){
    isValid = false
    messages.push("Email must contain both '@' and '.' characters.")
  }
-  if (passwordInput.length < 8){
+  if (password.length < 8){
     isValid = false
     messages.push("Password must be at least 8 characters long.")
   }

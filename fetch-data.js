7212764e0706e2408567a07dcb3a6441a303c739
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-10-18 22:06:57 +0100

    Added fetch api file

---
+++ fetch-data.js
@@ -0,0 +1,23 @@
+
+async function fetchUserDtaa() {
+  const apiUrl = 'https://jsonplaceholder.typicode.com/users'
+  const dataContainer = document.getElementById("api-data")
+  try {
+    const response = await fetch(apiUrl)
+    const users = await response.json()
+    dataContainer.innerHTML = ''
+    const userList = createElement("ul")
+    users.forEach(user => {
+      const li = document.createElement("li")
+      li.textContent = user.name
+      userList.appendChild("li")
+    });
+
+    dataContainer.appendChild("userList")
+  } catch (error) {
+    dataContainer.innerHTML = ''
+    console.error('Failed to load user data.')
+  }
+}
+
+document.addEventListener('DOMContentLoaded',fetchUserData)

ff6c2e71532f5500a5f88f8a3207b60d93204820
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-10-18 18:10:43 +0100

    all files added

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded',()=>{
- const form = document.getElementById('registration-form')
- const feedbackDiv = document.getElementById('feedback-form')
- form.addEventListener('submit',(event)=>{
+document.addEventListener("DOMContentLoaded",()=>{
+ const form = document.getElementById("registration-form")
+ const feedbackDiv = document.getElementById("feedback-form")
+ form.addEventListener("submit",(event)=>{
   event.preventDefault()
  })
- const usernameInput = document.getElementById('username').value.trim()
- const emailInput = document.getElementById('email').value.trim()
- const passwordInput = document.getElementById('password').value.trim()
+ const usernameInput = document.getElementById("username").value.trim()
+ const emailInput = document.getElementById("email").value.trim()
+ const passwordInput = document.getElementById("password").value.trim()
 
  const isValid = true
  const messages = []
 
- if (!email.include('@') || !email.include('.')){
+ if (!email.include("@") || !email.include(".")){
    isValid = false
    messages.push("Email must contain both '@' and '.' characters.")
  }
@@ -20,13 +20,13 @@ document.addEventListener('DOMContentLoaded',()=>{
     messages.push("Password must be at least 8 characters long.")
   }
 
-  feedbackDiv.style.display = 'block'
+  feedbackDiv.style.display = "block"
   if (isValid == true){
-    feedbackDiv.textContent = 'Registration successful!'
-    feedbackDiv.style.color = '#28a745'
+    feedbackDiv.textContent = "Registration successful!"
+    feedbackDiv.style.color = "#28a745"
   }else{
     feedbackDiv.innerHTML = messages.join("<br>")
-    feedbackDiv.style.color = '#dc3545'
+    feedbackDiv.style.color = "#dc3545"
   }
 })
  

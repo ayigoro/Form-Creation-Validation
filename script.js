1e8c3e9d5a0fcd9a4ad67f2d7b001a6f1ca099ff
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-10-18 18:14:27 +0100

    all files added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded",function(){
  const form = document.getElementById("registration-form")
- const feedbackDiv = document.getElementById("feedback-form")
+ const feedbackDiv = document.getElementById("form-feedback")
  form.addEventListener("submit",(event)=>{
   event.preventDefault()
  })
